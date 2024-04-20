22f9aaa3a3a3a54b3bf6ee4b19e69250593c066b
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-04-20 16:23:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7129687912:AAHO7MkLSXIHdmHpC4dmVvzB9nBZgmwRgy4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7012342197:AAFTFGxXV7vN0oVdopz3s9xM_GluDWltKEo")
     API_ID = int(os.environ.get("API_ID", "27803618"))
     API_HASH = os.environ.get("API_HASH", "2cdaef9643189f6bd9c7b31a70257356")
     AUTH_USERS = os.environ.get("AUTH_USERS", "745084259")
